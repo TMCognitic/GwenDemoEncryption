326f2e0f9c2706d51ead4f94ea848890db53404e
Thierry Morre <thierry.morre@cognitic.be>
2025-06-24 16:43:35 +0200

    Ajoutez des fichiers projet.

---
+++ DemoEncryption/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DemoEncryption;
+using System.Security.Cryptography;
+using System.Text;
+
+string dataToEncrypt = "Le contenu à crypter";
+
+byte[] publicKey;
+byte[] keys;
+byte[] cypher;
+
+using (RsaCrypto crypto = new RsaCrypto())
+{
+    publicKey = crypto.ExportKeys(false);
+    keys = crypto.ExportKeys(true);
+}
+
+
+using (RsaCrypto crypto = new RsaCrypto(publicKey))
+{
+    cypher = crypto.Encrypt(dataToEncrypt);
+}
+
+using (RsaCrypto crypto = new RsaCrypto(keys))
+{
+    Console.WriteLine(crypto.Decrypt(cypher));
+}
---
+++ DemoEncryption/RsaCrypto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEncryption
+{
+    public class RsaCrypto : IDisposable
+    {
+        private readonly RSACryptoServiceProvider _rsaCrypto;
+
+        public RsaCrypto(int keySize = 2048)
+        {
+            _rsaCrypto = new RSACryptoServiceProvider(keySize);
+        }
+
+        public RsaCrypto(byte[] keys)
+        {
+            _rsaCrypto = new RSACryptoServiceProvider();
+            _rsaCrypto.ImportCspBlob(keys);
+        }
+
+        public byte[] ExportKeys(bool includePrivateKey)
+        {
+            return _rsaCrypto.ExportCspBlob(includePrivateKey);
+        }
+
+        public byte[] Encrypt(string dataToEncrypt)
+        {
+            byte[] stringInByteArray = Encoding.Default.GetBytes(dataToEncrypt);
+            return _rsaCrypto.Encrypt(stringInByteArray, true);
+        }
+
+        public string Decrypt(byte[] cypher)
+        {
+            if(_rsaCrypto.PublicOnly)
+            {
+                throw new InvalidOperationException("The private key is needed for decrypt");
+            }
+
+            byte[] decryptedValue = _rsaCrypto.Decrypt(cypher, true);
+            return Encoding.Default.GetString(decryptedValue);
+        }
+
+        public void Dispose()
+        {
+            _rsaCrypto.Dispose();
+        }
+    }
+}
